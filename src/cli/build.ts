e911d7ce3530d97ce78c5df90768b57d34f2c97c
Lothar Bender <lothar.bender@sap.com>
2021-08-27 18:47:33 +0200

    fix cds-dbm build for @sap/cds >= 5.4     - requires build task definition     {         "use": "cds-dbm/dist/build/postgres-cf"         . . .     }
        An upcoming CDS version will support the registration of a custom build task provider.
        This allows you to determine the required custom build tasks based on the project context
        respectively CDS configuration instead of explicit definition.
        E.g. a custom build task provider can inspect the configured database kind in
        order to create a PostgressDB build task if "postgresql" has been configured.
        This will also allow the definition of a build task based on the "for" alias
        instead of the fully qualified module path provided by "use".

--- src/cli/build.ts
+++ src/cli/build.ts
@@ -1,30 +1,4 @@
-const path = require('path')
-const fs = require('@sap/cds-foss')('fs-extra')
-import * as cdsg from '@sap/cds'
-const cds = cdsg as any
-const { DbmBuildTaskFactory } = require('./../build/buildTaskFactory')
-const BuildTaskEngine = require('./../build/buildTaskEngine')
-
 exports.command = 'build'
 exports.desc = 'Generates build artifacts'
 exports.builder = {}
-exports.handler = async (argv: any) => {
-  const logger = global.console
-  const projectPath = path.resolve(argv.project || '.')
-
-  if (!fs.lstatSync(projectPath).isDirectory()) {
-    return Promise.reject(`Project [${projectPath}] does not exist`)
-  }
-
-  if (cds.env.features.snapi === 'runtime-only') cds.env.features.snapi = false
-  const buildOptions = _mergeOptions({ root: projectPath }, argv)
-  let tasks = await new DbmBuildTaskFactory(logger, cds).getTasks(buildOptions)
-  return new BuildTaskEngine(logger, cds).processTasks(tasks, buildOptions)
-}
-
-function _mergeOptions(buildOptions, options) {
-  buildOptions['log-level'] = options['log-level']
-  buildOptions.cli = options.cli
-  buildOptions.cmdOptions = options
-  return buildOptions
-}
+exports.handler = require('@sap/cds/bin/build')
