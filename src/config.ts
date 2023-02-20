ceff9e734d3b998e37661666aa3e4500fbefb8be
Md. Ashraful Alam <ajewel@schertech.com>
2023-02-20 14:16:51 +0600

    fix: failed to load model undefined issue

--- src/config.ts
+++ src/config.ts
@@ -63,7 +63,12 @@ interface configOptions {
 const config = async (service: string): Promise<configOptions> => {
   await cds.connect()
 
-  const serviceOptions = cds.env.requires[service]
+
+  /* Below code block for solve `[cds-dbm] - failed to load model undefined` issue. */
+  const _serviceOptions = cds.env.requires[service];
+  const _dbInfo = _serviceOptions.kind ? _serviceOptions.kind : {};
+  const serviceOptions = {..._serviceOptions, ...cds.env.requires[_dbInfo]};
+  /* end */
   
   // @ts-ignore
   const migrationOptions = cds.env.migrations[service]
